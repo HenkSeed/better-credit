b9073a593b43130a96348423cd4da7f808f62de1
HenkSeed <adaner@inbox.ru>
2024-08-19 21:40:51 +0700

    Made step1 section relativity

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,7 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
+import replaceSVGImages from "./replaceSVGImages.js";
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   // direction: 'vertical',
@@ -19,3 +21,5 @@ const swiper = new Swiper(".swiper", {
     clickable: true,
   },
 });
+
+replaceSVGImages();
